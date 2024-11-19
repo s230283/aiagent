9e239e72ccd545effe763095f3c3b882b535c3f3
s230283 <gimpo.s230283@ggh.goe.go.kr>
2024-11-19 21:58:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -29,10 +29,11 @@ with col2:
 st.subheader('모델활용')
 st.write('**** 다음을 입력하세요. 인공지능이 당신의 신용 등급을 알려드립니다!')
 
-a=st.number_input('소득 입력(달러)', value = 0.0)
-b=st.number_input('아이 수', value = 0)
+a = st.number_input('소득 입력(달러)', value = 0.0)
+b = st.number_input('아이 수 입력', value = 0)
+c = st.selectbox('집 유무 입력(없다: 0, 있다: 1', [0, 1])
 
 if st.button('신용등급판정'):
- input_data = [[a,b]]
+ input_data = [[a, b, c]]
  p=model.predict(input_data)
  st.write('인공지능한 당신의 신용 등급은', p)
